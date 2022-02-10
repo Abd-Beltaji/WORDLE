7a2f145fcaff87ab4c8033af299365c66de13a1b
Abdelrahman Elbeltaji <Abd.elbeltaji@gmail.com>
2022-02-11 00:38:49 +0200

    Updated body boilerplate 🧮

--- src/components/Words.tsx
+++ src/components/Words.tsx
@@ -1,7 +1,54 @@
 import React from 'react'
-
+import '@styles/components/Words.scss'
 const Words: React.FC<{}> = () => {
-	return <div className="dark:bg-slate-900 dark:text-white">Words</div>
+	return (
+		<div className="words dark:bg-slate-900 dark:text-white">
+			<table>
+				<tr>
+					<td>a</td>
+					<td>a</td>
+					<td>a</td>
+					<td>a</td>
+					<td>a</td>
+				</tr>
+				<tr>
+					<td>a</td>
+					<td>a</td>
+					<td>a</td>
+					<td>a</td>
+					<td>a</td>
+				</tr>
+				<tr>
+					<td>d</td>
+					<td>d</td>
+					<td>d</td>
+					<td>d</td>
+					<td>d</td>
+				</tr>
+				<tr>
+					<td>g</td>
+					<td>g</td>
+					<td>g</td>
+					<td>g</td>
+					<td>g</td>
+				</tr>
+				<tr>
+					<td>j</td>
+					<td>j</td>
+					<td>j</td>
+					<td>j</td>
+					<td>j</td>
+				</tr>
+				<tr>
+					<td>q</td>
+					<td>q</td>
+					<td>q</td>
+					<td>q</td>
+					<td>q</td>
+				</tr>
+			</table>
+		</div>
+	)
 }
 
 export default Words
